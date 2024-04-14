3c204f88e4a0d0116e40ba1cc6ed9a87167c2717
Rimjhim Gupta <rimjhimgupta786@gmail.com>
2024-04-14 13:59:12 +0530

    change file

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import { Link, NavLink } from 'react-router-dom';
 function Navbar () {
   
   
-  const userName =  JSON.parse(localStorage.getItem('user'));
+  // const userName =  JSON.parse(localStorage.getItem('user'));
   return (
     
     
@@ -12,7 +12,7 @@ function Navbar () {
   <div className="container">
     <Link className="navbar-brand" to="/"><img src="./image/it.jpeg" alt="" width='80px' /></Link>
 
-    <p className='text-center'>Welcome - {userName.name}</p>
+    {/* <p className='text-center'>Welcome - {userName.name}</p> */}
 
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
